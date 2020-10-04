232ca4105ab775b9cf8f52f5b0467312e7067859
AmrutanshuDash <68193810+AmrutanshuDash@users.noreply.github.com>
2020-10-04 17:18:56 +0530

    Changed the operation of the program
    I simply updated the program's operation from multiplication to division between two numbers.

--- test.c
+++ test.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main() {    
 
-    int number1, number2, mul;
+    int number1, number2, div;
     
     printf("Enter two integers: ");
     scanf("%d %d", &number1, &number2);
 
-    // calculating product
-    mul = number1 * number2;      
-    
-    printf("%d x %d = %d", number1, number2, mul);
+    // calculating division
+    mul = number1 / number2;      
+    // printing the output with division
+    printf("%d / %d = %d", number1, number2, div);
     return 0;
 }
